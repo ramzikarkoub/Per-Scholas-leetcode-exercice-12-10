7925d6f855c2f2df07c45ddb834475009c7e395d
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-10 11:08:02 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const findString = (word, char) => {
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === char) {
+      count++;
+    }
+  }
+  return count;
+};
+console.log(findString("hellllo", "l"));
+
+const minMax = (arr) => {
+  let max = arr[1];
+  let min = arr[1];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  let result = `the max and min are ${max} ${min}`;
+  return result;
+};
+let arr = [91, 5, 9, 2, 4, 73, 21];
+console.log(minMax(arr));
+
+const isTen = (num1, num2) => {
+  if (num1 === 10 || num2 === 10) {
+    return true;
+  } else if (num1 + num2 === 10) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(isTen(1));
